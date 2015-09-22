030a4ecf9845a7774f66ba3260fa185f318ba858
otto <oj.wilken@gmail.com>
2015-09-22 14:09:07 +0200

    fixes

--- webapp/README.md
+++
@@ -1,2 +0,0 @@
-# testAPP
-test
--- webapp/api.py
+++
@@ -1,9 +0,0 @@
-from tastypie.resources import ModelResource
-from customer_care.models import User
-
-__author__ = 'Otto'
-
-
-class EntryResource(ModelResource):
-    class Meta:
-        queryset = User.objects.all()
